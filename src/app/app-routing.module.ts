f6a6d2d9fbdbd3795c8612d36767dbb407cf392b
lourdesaa <loudaniele28@gmail.com>
2024-04-24 11:14:13 -0300

    creacion de primeros modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './modules/inicio/pages/inicio/inicio.component';
 
-const routes: Routes = [];
+//encargadas de tener todas las rutas de la pagina
+const routes: Routes = [
+  {
+    path: "", component: InicioComponent
+  },
+  // carga perezosa ->1 modulo
+  // loadChildren:indica una ruta hija
+  // ()=>import: ruta de donde viene el modulo
+  // .then:promesa/funcion asincronica
+  {
+    path: "", loadChildren: () => import('./modules/inicio/inicio.module').then(m => m.InicioModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
