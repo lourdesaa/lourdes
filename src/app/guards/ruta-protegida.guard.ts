346f48a8d64f0e96a767850ba803a8b941d0817d
lourdesaa <loudaniele28@gmail.com>
2024-10-16 10:14:14 -0300

    creacion de un guardian para proteger las rutas del administrador, creamos funciones para obtener el rol del usuario en 'servicioAuth', modificamos la vista de 'registro' e 'ininicio de sesion'

--- src/app/guards/ruta-protegida.guard.ts
+++ src/app/guards/ruta-protegida.guard.ts
@@ -1,5 +1,23 @@
 import { CanActivateFn } from '@angular/router';
 
+//inyeccion de servicios
+import { inject, Inject } from '@angular/core';
+
+import { AuthService } from '../modules/autentificacion/pages/services/auth.service';
+
+import { Router } from '@angular/router';
+
+import { map, switchMap, of, from } from 'rxjs';
+
 export const rutaProtegidaGuard: CanActivateFn = (route, state) => {
+  //inyectamos/instanciamos servicio de autentificacion
+  const servicioAuth = inject(AuthService)
+
+  //inyectamos/instanciamos servicio de rutas
+  const servicioRutas = inject(Router)
+
+  //especificamos el rol esperado en el guardian
+  const rolEsperado = "admin"
+
   return true;
 };
